133f2f99d2498752c9da47e2dd9be31605cdfe46
David Rueck <drueck@gmail.com>
2013-07-13 22:25:09 -0700

    Hash test word in initialize and extract method for select logic

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,22 +1,25 @@
 class Anagram
 
   def initialize(test_word)
-    @test_word = test_word
+    @hashed_test_word = anagram_hash(test_word)
   end
 
   def match(possible_anagrams)
-    hashed_test_word = anagram_hash(test_word)
     possible_anagrams.select do |word|
-      hashed_test_word == anagram_hash(word)
+      anagram_of_test_word?(word)
     end
   end
 
   private
 
-  attr_reader :test_word
+  attr_reader :hashed_test_word
 
   def anagram_hash(word)
     word.chars.sort
+  end
+
+  def anagram_of_test_word?(word)
+    hashed_test_word == anagram_hash(word)
   end
 
 end
