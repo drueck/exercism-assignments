8f93e906c7c7a5217dec0fd16971e4de9e0f53fa
David Rueck <drueck@gmail.com>
2013-07-18 16:54:28 -0700

    Initial version of elixir bob solution.
    First time writing elixir code!

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,28 @@
+defmodule Teenager do
+
+  def hey(msg) do
+    cond do
+      silent?(msg) ->
+        "Fine. Be that way."
+      shouting?(msg) ->
+        "Woah, chill out!"
+      asking?(msg) ->
+        "Sure."
+      true ->
+        "Whatever."
+    end
+  end
+
+  defp silent?(msg) do
+    msg == ""
+  end
+
+  defp shouting?(msg) do
+    msg == String.upcase(msg)
+  end
+
+  defp asking?(msg) do
+    String.ends_with?(msg, "?")
+  end
+
+end
