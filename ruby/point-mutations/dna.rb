4cf5273343f3dae8920704a8c33d96ee1c0f6dad
David Rueck <drueck@gmail.com>
2013-08-23 16:56:57 -0700

    Update point mutations solution to avoid extra check for nil
    I came across someone else's solution that restricted the length of the
    second strand to the shortest common length. I borrowed that idea here.
    I have already moved on past this exercise, but I figured I'd update it
    in my repo anyway. I like it a bit better.

--- ruby/point-mutations/dna.rb
+++ ruby/point-mutations/dna.rb
@@ -5,7 +5,7 @@ class DNA
   end
 
   def hamming_distance(other_sequence)
-    pairs_with_mutations(other_sequence).count
+    pairs_with_mutations(other_sequence).length
   end
 
   private
@@ -13,13 +13,18 @@ class DNA
   attr_reader :sequence
 
   def pairs_with_mutations(other_sequence)
-    sequence.chars.zip(other_sequence.chars).select do |n1, n2|
+    nucleotide_pairs(other_sequence).select do |n1, n2|
       mutation?(n1, n2)
     end
   end
 
+  def nucleotide_pairs(other_sequence)
+    max_common_length = [sequence.length, other_sequence.length].min
+    sequence.chars.zip(other_sequence.chars).take(max_common_length)
+  end
+
   def mutation?(n1, n2)
-    !n2.nil? && n1 != n2
+    n1 != n2
   end
 
 end
