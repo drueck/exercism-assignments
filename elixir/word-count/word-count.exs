6b7b84b3b2671991dc222abe6650e119678f117d
David Rueck <drueck@gmail.com>
2013-08-26 23:05:44 -0700

    Fix typo in the spelling of occurrences

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -2,14 +2,14 @@ defmodule Words do
 
   def count(phrase) do
     words = split_into_words(phrase)
-    HashDict.new words, fn (word) -> { word, occurences(words, word) } end
+    HashDict.new words, fn (word) -> { word, occurrences(words, word) } end
   end
 
   defp split_into_words(phrase) do
     String.downcase(phrase) |> String.split(%r/\W+/) |> Enum.reject(&1 == "")
   end
 
-  defp occurences(words, word) do
+  defp occurrences(words, word) do
     Enum.count(words, &1 == word)
   end
 
