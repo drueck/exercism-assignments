3b68afa4e04953b7da838e6385d1589f5e0d76c0
David Rueck <drueck@gmail.com>
2013-08-28 11:45:46 -0700

    Refactor anagrams? to use two implementations for each case.
    Thanks to @barrettclark for the feedback!

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,9 +4,8 @@ defmodule Anagram do
     Enum.filter(candidates, anagrams?(&1, word))
   end
 
-  defp anagrams?(word1, word2) do
-    word1 != word2 && sort_letters(word1) == sort_letters(word2)
-  end
+  defp anagrams?(word1, word1), do: false
+  defp anagrams?(word1, word2), do: sort_letters(word1) == sort_letters(word2)
 
   defp sort_letters(word) do
     String.downcase(word) |> String.codepoints |> Enum.sort
