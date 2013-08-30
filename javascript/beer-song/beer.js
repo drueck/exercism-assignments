bd0ac853bc25ad0c909171ec42cd167d66cb4c71
David Rueck <drueck@gmail.com>
2013-08-29 20:12:01 -0700

    Improve clarity of edge case formatting logic

--- javascript/beer-song/beer.js
+++ javascript/beer-song/beer.js
@@ -1,21 +1,3 @@
-function verse(bottles) {
-
-  if(bottles === 0) {
-    return "No more bottles of beer on the wall, no more bottles of beer.\n" +
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
-  }
-
-  if(bottles === 1) {
-    return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-      "Take it down and pass it around, no more bottles of beer on the wall.\n";
-  }
-
-  return bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    (bottles-1) + " bottle" + (bottles > 2 ? "s" : "") + " of beer on the wall.\n";
-
-}
-
 module.exports = {
 
   verse: verse,
@@ -33,3 +15,28 @@ module.exports = {
   }
 
 };
+
+function verse(bottles) {
+
+  if(bottles === 0) {
+    return "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+  }
+
+  if(bottles === 1) {
+    return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n";
+  }
+
+  var bottles_remaining = bottles - 1;
+
+  return bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    bottles_remaining + " " + pluralize_bottles(bottles_remaining) + " of beer on the wall.\n";
+
+}
+
+function pluralize_bottles(bottles) {
+  return bottles === 1 ? "bottle" : "bottles";
+}
+
