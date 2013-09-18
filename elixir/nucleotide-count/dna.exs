b555a425ea9fba993b00f1a01bfa81644e0bceee
David Rueck <drueck@gmail.com>
2013-09-18 13:07:29 -0700

    Use module attribute instead of private method.
    Both ways work fine, but for some reason having a module constant
    makes a little more sense to me. I don't love the @ syntax for it,
    so in that way I prefer using just the bare word `nucleotides`, but
    logically I like @nucleotides just a tiny bit more. 2 less lines of
    code, too.

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -1,15 +1,13 @@
 defmodule DNA do
 
+  @nucleotides [ ?A, ?T, ?C, ?G ]
+
   def count(strand, nucleotide) do
     Enum.count strand, &1 == nucleotide
   end
 
   def nucleotide_counts(strand) do
-    HashDict.new nucleotides, &{ &1, count(strand, &1) }
-  end
-
-  defp nucleotides do
-    [ ?A, ?T, ?C, ?G ]
+    HashDict.new @nucleotides, &{ &1, count(strand, &1) }
   end
 
 end
