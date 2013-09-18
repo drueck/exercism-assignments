d85f2f136afae216f48a078eea9b1bff87d2f826
David Rueck <drueck@gmail.com>
2013-09-17 18:48:46 -0700

    Remove parens. In this case I think it reads cleaner.

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -1,11 +1,11 @@
 defmodule DNA do
 
   def count(strand, nucleotide) do
-    Enum.count(strand, &1 == nucleotide)
+    Enum.count strand, &1 == nucleotide
   end
 
   def nucleotide_counts(strand) do
-    HashDict.new(nucleotides, fn n -> { n, count(strand, n) } end)
+    HashDict.new nucleotides, fn n -> { n, count(strand, n) } end
   end
 
   defp nucleotides do
