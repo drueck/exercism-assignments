a68d29eea98ea3df1b750d2d621f542f42ac13c8
David Rueck <drueck@gmail.com>
2013-09-17 18:21:29 -0700

    Reduce duplication

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -5,12 +5,11 @@ defmodule DNA do
   end
 
   def nucleotide_counts(strand) do
-    HashDict.new [
-      {?A, count(strand, ?A)},
-      {?T, count(strand, ?T)},
-      {?C, count(strand, ?C)},
-      {?G, count(strand, ?G)}
-    ]
+    HashDict.new(nucleotides, fn n -> { n, count(strand, n) } end)
+  end
+
+  defp nucleotides do
+    [ ?A, ?T, ?C, ?G ]
   end
 
 end
