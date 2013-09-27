41af59ba656a964ccc10b988f7030c6fd7cbdfd3
David Rueck <drueck@gmail.com>
2013-09-27 15:53:22 -0700

    Get rid of the `homologous_pairs` function. Use name/arity.
    Since `List.zip` is so straightforward, `homologous_pairs` kind of lost
    it's self-documenting purpose. I still think `mutation?` is nice.
    
    Using the name/arity method of refercing the function is kinda nice. I
    want to get used to using that method for cases where the function takes
    more than one param, as it's less verbose and the verbosity is not
    helpful in understanding what's going on because the params have no
    meaningful names anyway.

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -1,12 +1,7 @@
 defmodule DNA do
 
   def hamming_distance(strand1, strand2) do
-    homologous_pairs(strand1, strand2)
-      |> Enum.count(&mutation?(&1))
-  end
-
-  defp homologous_pairs(strand1, strand2) do
-    List.zip([strand1, strand2])
+    List.zip([strand1, strand2]) |> Enum.count(&mutation?/1)
   end
 
   defp mutation?({n1, n2}), do: n1 != n2
