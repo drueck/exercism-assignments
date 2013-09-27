34e757dd438582fd7c99e0cbdbdef3f56e6b7e95
David Rueck <drueck@gmail.com>
2013-09-27 14:03:28 -0700

    Use List.zip instead of Enum.zip
    List.zip goes with the shortest common length! Nice!

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -6,8 +6,7 @@ defmodule DNA do
   end
 
   defp homologous_pairs(strand1, strand2) do
-    [shorter, longer] = Enum.sort([strand1, strand2], &(Enum.count(&1) < Enum.count(&2)))
-    Enum.zip(shorter, longer)
+    List.zip([strand1, strand2])
   end
 
   defp mutation?({n1, n2}), do: n1 != n2
