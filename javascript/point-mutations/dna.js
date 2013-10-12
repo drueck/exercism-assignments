ba5529fd2a292f118971ea776c81425f9270e395
David Rueck <drueck@gmail.com>
2013-10-12 13:29:56 -0700

    Use Math.min to find the comparable length--much simpler!

--- javascript/point-mutations/dna.js
+++ javascript/point-mutations/dna.js
@@ -1,19 +1,11 @@
 module.exports = function DNA(strand) {
 
-  function comparableLength(strand, otherStrand) {
-    if (strand.length < otherStrand.length) {
-      return strand.length;
-    } else {
-      return otherStrand.length;
-    }
-  }
-
   return {
 
     hammingDistance: function(otherStrand) {
       var mutations = 0;
-      var commonLength = comparableLength(strand, otherStrand);
-      for (i = 0; i < commonLength; i++) {
+      var comparableLength = Math.min(strand.length, otherStrand.length);
+      for (i = 0; i < comparableLength; i++) {
         if (strand[i] !== otherStrand[i]) {
           mutations++;
         }
