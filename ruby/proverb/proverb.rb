fd4d345e034ae9d5a35101cee1b25765ac4c755a
David Rueck <drueck@gmail.com>
2013-12-23 15:41:03 -0800

    Update naming a bit to be a little more meaningful

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,7 +1,7 @@
 class Proverb
 
-  def initialize(*items, qualifier: "")
-    @items = items
+  def initialize(*things, qualifier: "")
+    @things = things
     @qualifier = qualifier
   end
 
@@ -11,26 +11,26 @@ class Proverb
 
   private
 
-  attr_reader :items, :qualifier
+  attr_reader :things, :qualifier
 
   def lines
     standard_lines << final_line
   end
 
   def standard_lines
-    items.each_cons(2).map { |(item1, item2)| line(item1, item2) }
+    things.each_cons(2).map { |(smaller, larger)| line(smaller, larger) }
   end
 
-  def line(item1, item2)
-    "For want of a #{item1} the #{item2} was lost."
+  def line(smaller_thing, larger_thing)
+    "For want of a #{smaller_thing} the #{larger_thing} was lost."
   end
 
   def final_line
-    "And all for the want of a #{ultimate_cause}."
+    "And all for the want of a #{smallest_thing}."
   end
 
-  def ultimate_cause
-    "#{qualifier} #{items.first}".lstrip
+  def smallest_thing
+    "#{qualifier} #{things.first}".lstrip
   end
 
 end
