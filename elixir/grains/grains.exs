aafe5dd3000dc4ee9c2054d85340a3cf1355a884
David Rueck <drueck@gmail.com>
2013-12-25 14:33:01 -0800

    Fix indenting inconsistencies

--- elixir/grains/grains.exs
+++ elixir/grains/grains.exs
@@ -2,19 +2,19 @@ defmodule Grains do
 
   use Bitwise
 
-	@doc """
-	Calculate two to the power of the input minus one.
-	"""
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
 
-	@spec square(pos_integer) :: pos_integer
-	def square(number), do: two_to_the_power_of(number - 1)
+  @spec square(pos_integer) :: pos_integer
+  def square(number), do: two_to_the_power_of(number - 1)
 
-	@doc """
-	Adds square of each number from 1 to 64.
-	"""
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
 
-	@spec total :: pos_integer
-	def total, do: do_total(64)
+  @spec total :: pos_integer
+  def total, do: do_total(64)
 
   defp do_total(1), do: square(1)
   defp do_total(n), do: square(n) + do_total(n - 1)
