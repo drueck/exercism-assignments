e6a4fa54d0e9e64636f4956cf636870452b8d90f
David Rueck <drueck@gmail.com>
2014-01-06 23:46:13 -0800

    Sort the sides to simplify required tests

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -8,22 +8,12 @@ defmodule Triangle do
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
 
   def kind(a, b, c) do
+    [a, b, c] = Enum.sort([a, b, c])
     cond do
-      any_non_positive?([a, b, c]) ->
-        { :error, "all side lengths must be positive" }
-      violates_triangle_inequality?(a, b, c) ->
-        { :error, "side lengths violate triangle inequality" }
-      true ->
-        { :ok, triangle_type([a, b, c]) }
+      a <= 0 ->     { :error, "all side lengths must be positive" }
+      a + b <= c -> { :error, "side lengths violate triangle inequality" }
+      true ->       { :ok, triangle_type([a, b, c]) }
     end
-  end
-
-  defp any_non_positive?(sides) do
-    Enum.any?(sides, &1 <= 0)
-  end
-
-  defp violates_triangle_inequality?(a, b, c) do
-    a + b <= c || a + c <= b || b + c <= a
   end
 
   defp triangle_type(sides) do
