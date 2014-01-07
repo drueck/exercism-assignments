4158ef141fe985ddc3f30b623239681b79a64abc
David Rueck <drueck@gmail.com>
2014-01-07 10:42:04 -0800

    Simplify tests for triangle type, move them into kind function

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -10,22 +10,12 @@ defmodule Triangle do
   def kind(a, b, c) do
     [a, b, c] = Enum.sort([a, b, c])
     cond do
-      a <= 0 ->     { :error, "all side lengths must be positive" }
-      a + b <= c -> { :error, "side lengths violate triangle inequality" }
-      true ->       { :ok, triangle_type([a, b, c]) }
+      a <= 0 ->           { :error, "all side lengths must be positive" }
+      a + b <= c ->       { :error, "side lengths violate triangle inequality" }
+      a == b && b == c -> { :ok, :equilateral }
+      a == b || b == c -> { :ok, :isosceles }
+      true ->             { :ok, :scalene }
     end
-  end
-
-  defp triangle_type(sides) do
-    case unique_sides(sides) do
-      1 -> :equilateral
-      2 -> :isosceles
-      3 -> :scalene
-    end
-  end
-
-  defp unique_sides(sides) do
-    HashSet.new(sides) |> Set.size
   end
 
 end
