2907ac4a03befb657e62af5a889a1da8f99a5bdf
David Rueck <drueck@gmail.com>
2014-01-08 15:48:30 -0800

    Initial attempt at elixir list-ops

---
+++ elixir/list-ops/list_ops.exs
@@ -0,0 +1,47 @@
+defmodule ListOps do
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    reduce(l, 0, fn(acc, _) -> acc + 1 end)
+  end
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+    reduce(l, [], fn(acc, x) -> [x | acc] end)
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+    reduce(l, [], fn(acc, x) -> [f.(x) | acc] end)
+    |> reverse
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+    reduce(l, [], fn(acc, x) ->
+      if f.(x) do [x | acc] else acc end
+    end)
+    |> reverse
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, ((acc, any) -> acc)) :: acc
+  def reduce([], acc, _), do: acc
+  def reduce([h | t], acc, f) do
+    reduce(t, f.(acc, h), f)
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+    reduce(reverse(a), b, fn(acc, x) -> [x | acc] end)
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+    reduce(ll, [], fn(acc, l) ->
+      reduce(l, acc, fn(acc, x) -> [x | acc] end)
+    end)
+    |> reverse
+  end
+
+end
