2c076be4c1cf99b29cd46976c3c864cbd8d6382b
David Rueck <drueck@gmail.com>
2014-01-08 10:40:18 -0800

    Eliminate magic number 64

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,11 +1,13 @@
 class Grains
 
+  SQUARES_ON_BOARD = 64
+
   def square(n)
     two_to_the_power_of(n - 1)
   end
 
   def total
-    (1..64).reduce(0) { |sum, n| sum + square(n) }
+    (1..SQUARES_ON_BOARD).reduce(0) { |sum, n| sum + square(n) }
   end
 
   private
