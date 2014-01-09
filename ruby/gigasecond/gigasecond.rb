7f65a93df83908d20889dbf82868c47a0f836aad
David Rueck <drueck@gmail.com>
2014-01-09 11:20:16 -0800

    Switch to constant instead of class instance variable

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,13 +1,13 @@
 class Gigasecond
 
-  @@a_billion_seconds_in_days = 1_000_000_000 / (60 * 60 * 24)
+  IN_DAYS = 1_000_000_000 / (60 * 60 * 24)
 
   def initialize(from_date)
     @from_date = from_date
   end
 
   def date
-    from_date + @@a_billion_seconds_in_days
+    from_date + Gigasecond::IN_DAYS
   end
 
   private
