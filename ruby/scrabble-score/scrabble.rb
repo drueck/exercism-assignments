6591b813619ae26000a6cfd06df415b83e87af91
David Rueck <drueck@gmail.com>
2014-01-14 12:06:36 -0800

    Represent scores table more cleanly and transform to desired form

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,14 +1,15 @@
 class Scrabble
 
   LETTER_VALUES = {
-    "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1,
-    "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1,
-    "D" => 2, "G" => 2,
-    "B" => 3, "C" => 3, "M" => 3, "P" => 3,
-    "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4,
-    "K" => 5,
-    "J" => 8, "X" => 8,
-    "Q" => 10, "Z" => 10
+    1 => %w{ A E I O U L N R S T },
+    2 => %w{ D G },
+    3 => %w{ B C M P },
+    4 => %w{ F H V W Y },
+    5 => %w{ K },
+    8 => %w{ J X },
+    10 => %w{ Q Z }
+  }.each_with_object({}) { |(score, letters), values|
+    letters.each { |letter| values[letter] = score }
   }
 
   def self.score(word)
@@ -30,5 +31,6 @@ class Scrabble
   def value(letter)
     LETTER_VALUES.fetch(letter.upcase) { 0 }
   end
+
 
 end
