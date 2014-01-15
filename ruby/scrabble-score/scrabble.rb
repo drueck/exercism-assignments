9b094d7338beb66df79a182c76b5c4af3b03ae0e
David Rueck <drueck@gmail.com>
2014-01-14 16:28:30 -0800

    Make LETTER_VALUES private, wrap in private method because barewords

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,17 +1,5 @@
 class Scrabble
 
-  LETTER_VALUES = {
-    1 => %w{ A E I O U L N R S T },
-    2 => %w{ D G },
-    3 => %w{ B C M P },
-    4 => %w{ F H V W Y },
-    5 => %w{ K },
-    8 => %w{ J X },
-    10 => %w{ Q Z }
-  }.each_with_object({}) { |(score, letters), values|
-    letters.each { |letter| values[letter] = score }
-  }
-
   def self.score(word)
     new(word).score
   end
@@ -29,8 +17,24 @@ class Scrabble
   attr_reader :word
 
   def value(letter)
-    LETTER_VALUES.fetch(letter.upcase) { 0 }
+    letter_values.fetch(letter.upcase) { 0 }
   end
 
+  def letter_values
+    LETTER_VALUES
+  end
+
+  LETTER_VALUES = {
+    1 => %w{ A E I O U L N R S T },
+    2 => %w{ D G },
+    3 => %w{ B C M P },
+    4 => %w{ F H V W Y },
+    5 => %w{ K },
+    8 => %w{ J X },
+    10 => %w{ Q Z }
+  }.each_with_object({}) { |(score, letters), values|
+    letters.each { |letter| values[letter] = score }
+  }
+  private_constant :LETTER_VALUES
 
 end
