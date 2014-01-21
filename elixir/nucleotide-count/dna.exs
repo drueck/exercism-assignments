476e3e06015cf3c2eeb2f3707c6b77f72a8d946d
David Rueck <drueck@gmail.com>
2014-01-21 12:25:16 -0800

    Update elixir nucleotide count tests and update solution

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -3,7 +3,7 @@ defmodule DNA do
   @nucleotides [ ?A, ?T, ?C, ?G ]
 
   def count(strand, nucleotide) do
-    Enum.count strand, &1 == nucleotide
+    Enum.count strand, &(&1 == nucleotide)
   end
 
   def nucleotide_counts(strand) do
