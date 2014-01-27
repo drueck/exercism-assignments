e2c802cacc0e39e4b0833c7c718f9fce240e33fe
David Rueck <drueck@gmail.com>
2014-01-27 13:54:40 -0800

    Update js nucleotide counts to only pass over the sequence once
    Use reduce directly on Strings using .call instead of splitting into an
    Array first. Also go with the constructor + prototype pattern as it
    feels appropriate here. It's not exposing anything publicly that doesn't
    need to be exposed, and it's optimizing for amount of work required to
    create a new DNA instance.

--- javascript/nucleotide-count/nucleotide.js
+++ javascript/nucleotide-count/nucleotide.js
@@ -1,38 +1,42 @@
+var DNA_NUCLEOTIDES = "ATCG";
+var ALL_NUCLEOTIDES = DNA_NUCLEOTIDES + "U";
+var reduce = Array.prototype.reduce;
+
+
 function DNA(sequence) {
-
-  var ALL_NUCLEOTIDES = [ "A", "T", "C", "G", "U" ];
-
-  return {
-    count: count,
-    nucleotideCounts: {
-      A: count("A"),
-      T: count("T"),
-      C: count("C"),
-      G: count("G")
-    },
-  };
-
-  function count(nucleotide) {
-    validateNucleotide(nucleotide);
-    return nucleotides().filter(function(n) {
-      return n === nucleotide;
-    }).length;
-  }
-
-  function validateNucleotide(letter) {
-    if(!isValidNucleotide(letter)) {
-      throw new Error("Invalid Nucleotide");
-    }
-  }
-
-  function isValidNucleotide(letter) {
-    return ALL_NUCLEOTIDES.indexOf(letter) != -1;
-  }
-
-  function nucleotides() {
-    return sequence.split("");
-  }
-
+  this.nucleotideCounts = nucleotideCounts(sequence);
 }
 
+DNA.prototype.count = function(nucleotide) {
+  validateNucleotide(nucleotide);
+  return this.nucleotideCounts[nucleotide] || 0;
+};
+
 module.exports = DNA;
+
+
+function nucleotideCounts(sequence) {
+  return reduce.call(sequence, function(counts, nucleotide) {
+    if(counts[nucleotide] >= 0) {
+      counts[nucleotide]++;
+    }
+    return counts;
+  }, initialCounts());
+}
+
+function initialCounts() {
+  return reduce.call(DNA_NUCLEOTIDES, function(counts, nucleotide) {
+    counts[nucleotide] = 0;
+    return counts;
+  }, {});
+}
+
+function validateNucleotide(letter) {
+  if(!isValidNucleotide(letter)) {
+    throw new Error("Invalid Nucleotide");
+  }
+}
+
+function isValidNucleotide(letter) {
+  return ALL_NUCLEOTIDES.indexOf(letter) != -1;
+}
