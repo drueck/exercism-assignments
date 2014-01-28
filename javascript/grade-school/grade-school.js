53482d1ed9f9aa779dbf2a015f482fc19c01db20
David Rueck <drueck@gmail.com>
2014-01-28 11:55:48 -0800

    Use constuctor and prototype pattern and make sort non-mutating
    Since the tests unfortunately force exposing `db`, there is really no
    private-information hiding reason not to use the constructor pattern, so
    I decided to make that change. Also, I decided to make sort a pure query
    method so that it does not mutate the internal state, even though that
    state can easily be mutated via the `db` property.
    
    Note that it would be better if `sort` was named `sorted` or something
    like that so that it was clear that it is a query method. :/

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,36 +1,33 @@
-module.exports = function School(name) {
+function School(name) {
+  this.db = {};
+}
 
-  var db = {};
-
-  function add(student, grade) {
-    if(db[grade]) {
-      db[grade].push(student);
-    } else {
-      db[grade] = [student];
-    }
+School.prototype.add = function(student, grade) {
+  if(this.db[grade]) {
+    this.db[grade].push(student);
+  } else {
+    this.db[grade] = [student];
   }
-
-  function grade(level) {
-    return db[level] || [];
-  }
-
-  function grades() {
-    return Object.keys(db).sort();
-  }
-
-  function sort() {
-    var sortedDb = {};
-    grades().forEach(function(grade) {
-      sortedDb[grade] = db[grade].sort();
-    });
-    return sortedDb;
-  }
-
-  return {
-    db: db,
-    add: add,
-    grade: grade,
-    sort: sort
-  };
-
 };
+
+School.prototype.grade = function(level) {
+  return this.db[level] || [];
+};
+
+School.prototype.sort = function() {
+  var self = this;
+  return sortedGrades(self.db).reduce(function(sorted, grade) {
+    sorted[grade] = clone(self.db[grade]).sort();
+    return sorted;
+  }, {});
+};
+
+module.exports = School;
+
+function sortedGrades(db) {
+  return Object.keys(db).sort();
+}
+
+function clone(array) {
+  return array.slice();
+}
