ed5bef026f9c7b3fba389f4031edcd52ccaaf42a
David Rueck <drueck@gmail.com>
2014-01-28 19:21:08 -0800

    Initial attempt at elixir prime factors

---
+++ elixir/prime-factors/prime_factors.exs
@@ -0,0 +1,23 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest.
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(number) do
+    do_factors(number, 2, []) |> Enum.reverse
+  end
+
+  defp do_factors(1, _, factors), do: factors
+  defp do_factors(number, divisor, factors) do
+    if rem(number, divisor) == 0 do
+      do_factors(div(number, divisor), divisor, [ divisor | factors ])
+    else
+      do_factors(number, divisor + 1, factors)
+    end
+  end
+end
