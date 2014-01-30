0759fbad7bb961f7220f06ff9db8390c1f232f61
David Rueck <drueck@gmail.com>
2014-01-29 23:55:58 -0800

    Remove the is_prime? function and its use
    It turns out it wasn't necessary because the algorithm of the Sieve of
    Eratosthenes implies that the next item in the candidates list will
    always be a prime, so you don't have to test for that. You just add it
    to the list of primes and then reject all multiples of that value from
    the remaining list of candidates.

--- elixir/sieve/sieve.exs
+++ elixir/sieve/sieve.exs
@@ -5,25 +5,13 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
-    do_primes(Enum.to_list(2..limit), []) |> Enum.reverse
+    Enum.to_list(2..limit) |> do_primes([]) |> Enum.reverse
   end
 
   defp do_primes([], primes), do: primes
   defp do_primes([ candidate | rest ], primes) do
-    if is_prime?(candidate, primes) do
-      candidates = Enum.reject(rest, &(rem(&1, candidate) == 0))
-      do_primes(candidates, [ candidate | primes ])
-    else
-      do_primes(rest, primes)
-    end
-  end
-
-  @doc """
-  Checks if a given number is prime based on a given list of known primes.
-  """
-  @spec is_prime?(non_neg_integer, [non_neg_integer]) :: boolean
-  defp is_prime?(number, primes) do
-    Enum.all?(primes, &(number == &1 || rem(number, &1) != 0))
+    candidates = Enum.reject(rest, &(rem(&1, candidate) == 0))
+    do_primes(candidates, [ candidate | primes ])
   end
 
 end
