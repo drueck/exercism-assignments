dffa9937d5f079b00f5c6531b096626d0bb3bb33
David Rueck <drueck@gmail.com>
2014-02-04 22:55:23 -0800

    Initial attempt at elixir sum of multiples

---
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -0,0 +1,18 @@
+defmodule SumOfMultiples do
+
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+
+  The default factors are 3 and 5.
+  """
+  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+  def to(limit, factors // [3, 5]) do
+    Stream.filter(1..(limit - 1), &multiple_of(&1, factors))
+      |> Enum.reduce(0, &(&1 + &2))
+  end
+
+  defp multiple_of(number, factors) do
+    Enum.any?(factors, &(rem(number, &1) == 0))
+  end
+
+end
